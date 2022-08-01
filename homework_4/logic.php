f7a9abf7b5fb8f0bcf4654cba173c989f7d0211c
igor_pont <pontigor11@gmail.com>
2022-08-01 11:56:11 +0300

    add homework_4

---
+++ homework_4/logic.php
@@ -0,0 +1,2 @@
+<?php
+$work_exper = array('Project manager', 'Jan 2020', 'Current', 'The manager is responsible for the implementation of a project or idea within a certain time frame by his team, using specific resources and in the quality required by the customer.');
